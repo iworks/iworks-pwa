d41653236e32f03c707692a04abd0ff9ee957410
Marcin Pietrzak <marcin@iworks.pl>
2021-02-26 19:01:02 +0100

    fix patch

--- assets/scripts/frontend.js
+++ assets/scripts/frontend.js
@@ -4,7 +4,7 @@
  */
 if ('serviceWorker' in navigator) {
     console.log('CLIENT: service worker registration in progress. serviceWorker');
-    navigator.serviceWorker.register( '/iworks-pwa-service-worker.js.php' ).then(function() {
+    navigator.serviceWorker.register( '/iworks-pwa-service-worker-js' ).then(function() {
         console.log('CLIENT: service worker registration complete.');
     }, function() {
         console.log('CLIENT: service worker registration failure.');
