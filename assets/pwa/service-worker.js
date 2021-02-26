7b716393f0ee5af777cecca2cb42a501b15b62ce
Marcin Pietrzak <marcin@iworks.pl>
2021-02-26 18:25:54 +0100

    init

--- assets/pwa/service-worker.js
+++ assets/pwa/service-worker.js
@@ -1,41 +1,76 @@
-// nazywamy nasze cache- w zmiennej będzie wygodniej
-const MY_CACHE = 'cache-name';
-// w tej tablicy lądują wszystkie pliki, które chcemy dodać do cache
-const MY_FILES = [
-    '/css/file.css',
-    '/css/images/file.png',
-    '/css/fonts/file.woff',
-    '/js/file.js'
-];
+opyright 2015, 2019 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
 
-// instalujemy nasz service worker
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(MY_CACHE).then(function(cache) {
-            return cache.addAll(MY_FILES);
-        })
-    );
+// Incrementing OFFLINE_VERSION will kick off the install event and force
+// previously cached resources to be updated from the network.
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = 'offline';
+// Customize this with a different URL if needed.
+const OFFLINE_URL = 'offline.html';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    // Setting {cache: 'reload'} in the new request will ensure that the response
+    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
+    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
+  })());
 });
-// po aktywacji chcę skasować wszystkie cache w naszej domenie, które nie są naszym cache (to opcjonalne)
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.filter(function(cacheName) {
-                    return cacheName !== MY_CACHE;
-                }).map(function(cacheName) {
-                    return caches.delete(cacheName);
-                })
-            );
-        })
-    );
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    // Enable navigation preload if it's supported.
+    // See https://developers.google.com/web/updates/2017/02/navigation-preload
+    if ('navigationPreload' in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
+  })());
+
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
 });
-// strategia 'Network falling back to cache'
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        fetch(event.request).catch(function() {
-            return caches.match(event.request);
-        })
-    );
+
+self.addEventListener('fetch', (event) => {
+  // We only want to call event.respondWith() if this is a navigation request
+  // for an HTML page.
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        // First, try to use the navigation preload response if it's supported.
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
+
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+        // catch is only triggered if an exception is thrown, which is likely
+        // due to a network error.
+        // If fetch() returns a valid HTTP response with a response code in
+        // the 4xx or 5xx range, the catch() will NOT be called.
+        console.log('Fetch failed; returning offline page instead.', error);
+
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(OFFLINE_URL);
+        return cachedResponse;
+      }
+    })());
+  }
+
+  // If our if() condition is false, then this fetch handler won't intercept the
+  // request. If there are any other fetch handlers registered, they will get a
+  // chance to call event.respondWith(). If no fetch handlers call
+  // event.respondWith(), the request will be handled by the browser as if there
+  // were no service worker involvement.
 });
 
--- assets/scripts/frontend.js
+++ assets/scripts/frontend.js
@@ -4,7 +4,7 @@
  */
 if ('serviceWorker' in navigator) {
     console.log('CLIENT: service worker registration in progress. serviceWorker');
-    navigator.serviceWorker.register( window.iworks_pwa.pwa.root + 'service-worker.php').then(function() {
+    navigator.serviceWorker.register( window.iworks_pwa.pwa.root + 'service-worker.js').then(function() {
         console.log('CLIENT: service worker registration complete.');
     }, function() {
         console.log('CLIENT: service worker registration failure.');
