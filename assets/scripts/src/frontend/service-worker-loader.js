6be20ae4a063496429ae6fc428eed0fda90d213c
Marcin Pietrzak <marcin@iworks.pl>
2021-03-05 12:31:00 +0100

    remove debug

--- assets/scripts/src/frontend/service-worker-loader.js
+++ assets/scripts/src/frontend/service-worker-loader.js
@@ -1,8 +1,8 @@
 if ('serviceWorker' in navigator) {
-    console.log('CLIENT: service worker registration in progress. serviceWorker');
+    // console.log('CLIENT: service worker registration in progress. serviceWorker');
     navigator.serviceWorker.register( '/iworks-pwa-service-worker-js' ).then(function() {
-        console.log('CLIENT: service worker registration complete.');
+        // console.log('CLIENT: service worker registration complete.');
     }, function() {
-        console.log('CLIENT: service worker registration failure.');
+        // console.log('CLIENT: service worker registration failure.');
     });
 }
