a2d0cc4ccc0e8ca90ec50256f27c8aa7fead40df
Marcin Pietrzak <marcin@iworks.pl>
2025-09-01 11:41:00 +0200

    prepare new version

--- assets/scripts/frontend.js
+++ assets/scripts/frontend.js
@@ -1,4 +1,4 @@
-/*! PWA — easy way to Progressive Web App - 1.7.7
+/*! PWA — easy way to Progressive Web App - 1.7.8
  * https://github.com/iworks/iworks-pwa
  * Copyright (c) 2025; * Licensed GPL-3.0 */
 window.addEventListener('load', function(event) {
