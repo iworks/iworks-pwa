32935b842b8bebcf56f8632f7a0c698668d53eff
Marcin Pietrzak <marcin@iworks.pl>
2026-07-20 12:47:00 +0200

    update build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,7 @@ module.exports = function(grunt) {
 
 	var buildtime = new Date().toISOString();
 	var buildyear = 1900 + new Date().getYear();
+	var buildtimestamp = new Date().getTime();
 
 	/**
 	 * excludes
@@ -92,6 +93,9 @@ module.exports = function(grunt) {
 			match: /AUTHOR_URI/g,
 			replace: '<%= pkg.author[0].uri %>'
 		}, {
+			match: /BUILDTIMESTAMP/g,
+			replace: buildtimestamp
+		}, {
 			match: /BUILDTIME/g,
 			replace: buildtime
 		}, {
@@ -147,6 +151,7 @@ module.exports = function(grunt) {
 		// Regex patterns to exclude from transation.
 		translation: {
 			ignore_files: [
+				'README.md',
 				'.git*',
 				'includes/external/.*', // External libraries.
 				'node_modules/.*',
@@ -170,10 +175,10 @@ module.exports = function(grunt) {
 			options: {
 				stripBanners: true,
 				banner: '/*! <%= pkg.title %> - <%= pkg.version %>\n' +
-				' * <%= pkg.homepage %>\n' +
-				' * Copyright (c) <%= grunt.template.today("yyyy") %>;' +
-				' * Licensed <%= pkg.license %>' +
-				' */\n'
+					' * <%= pkg.homepage %>\n' +
+					' * Copyright (c) <%= grunt.template.today("yyyy") %>\n' +
+					' * Licensed <%= pkg.license %>' +
+					' */\n'
 			},
 			scripts: {
 				files: conf.js_files_concat
@@ -218,10 +223,10 @@ module.exports = function(grunt) {
 				}],
 				options: {
 					banner: '/*! <%= pkg.title %> - <%= pkg.version %>\n' +
-					' * <%= pkg.homepage %>\n' +
-					' * Copyright (c) <%= grunt.template.today("yyyy") %>;' +
-					' * Licensed <%= pkg.license %>' +
-					' */\n',
+						' * <%= pkg.homepage %>\n' +
+						' * Copyright (c) <%= grunt.template.today("yyyy") %>;\n' +
+						' * Licensed <%= pkg.license %>' +
+						' */\n',
 					mangle: {
 						except: ['jQuery']
 					}
@@ -247,7 +252,6 @@ module.exports = function(grunt) {
 				testsuite: 'default',
 				configuration: 'tests/php/phpunit.xml',
 				colors: true,
-				tap: true,
 				staticBackup: false,
 				noGlobalsBackup: false
 			}
@@ -276,10 +280,10 @@ module.exports = function(grunt) {
 		cssmin: {
 			options: {
 				banner: '/*! <%= pkg.title %> - <%= pkg.version %>\n' +
-				' * <%= pkg.homepage %>\n' +
-				' * Copyright (c) <%= grunt.template.today("yyyy") %>;\n' +
-				' * Licensed <%= pkg.license %>' +
-				' */\n',
+					' * <%= pkg.homepage %>\n' +
+					' * Copyright (c) <%= grunt.template.today("yyyy") %>;\n' +
+					' * Licensed <%= pkg.license %>' +
+					' */\n',
 				mergeIntoShorthands: false
 			},
 			minify: {
@@ -354,10 +358,9 @@ module.exports = function(grunt) {
 						'report-msgid-bugs-to': 'http://iworks.pl',
 						'x-poedit-keywordslist': true // Include a list of all possible gettext functions.
 					},
-					exclude: ['node_modules', '.git', '.sass-cache', 'release'],
 					type: 'wp-plugin',
-					updateTimestamp: true,
-					updatePoFiles: true
+					updateTimestamp: true, // Whether the POT-Creation-Date should be updated without other changes.
+					updatePoFiles: true // Whether to update PO files in the same directory as the POT file.
 				}
 			}
 		},
@@ -435,7 +438,7 @@ module.exports = function(grunt) {
 
 		checktextdomain: {
 			options: {
-				text_domain: ['<%= pkg.name %>', 'IWORKS_RATE_TEXTDOMAIN', 'IWORKS_OPTIONS_TEXTDOMAIN'],
+				text_domain: ['<%= pkg.name %>', 'PLUGIN_NAME', 'IWORKS_RATE_TEXTDOMAIN', 'IWORKS_OPTIONS_TEXTDOMAIN'],
 				keywords: [ //List keyword specifications
 					'__:1,2d',
 					'_e:1,2d',
